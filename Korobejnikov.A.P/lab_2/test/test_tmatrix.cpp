48ae9a815ac6cfd8f002af4a27d7beaf7bb17fff
KorobejnikovAP <armok2001@mail.ru>
2020-11-17 15:15:28 +0300

    lab2

---
+++ Korobejnikov.A.P/lab_2/test/test_tmatrix.cpp
@@ -0,0 +1,166 @@
+#include "utmatrix.h"
+
+#include <gtest.h>
+
+TEST(TMatrix, can_create_matrix_with_positive_length)
+{
+  ASSERT_NO_THROW(TMatrix<int> m(5));
+}
+
+TEST(TMatrix, cant_create_too_large_matrix)
+{
+  ASSERT_ANY_THROW(TMatrix<int> m(MAX_MATRIX_SIZE + 1));
+}
+
+TEST(TMatrix, throws_when_create_matrix_with_negative_length)
+{
+  ASSERT_ANY_THROW(TMatrix<int> m(-5));
+}
+
+TEST(TMatrix, can_create_copied_matrix)
+{
+  TMatrix<int> m(5);
+
+  ASSERT_NO_THROW(TMatrix<int> m1(m));
+}
+
+TEST(TMatrix, copied_matrix_is_equal_to_source_one)
+{
+	TMatrix<int> m(10);
+	TMatrix<int> m1(m);
+
+	EXPECT_EQ(m, m1);
+
+}
+
+TEST(TMatrix, copied_matrix_has_its_own_memory)
+{
+	TMatrix<int> m(10);
+	TMatrix<int> m1(m);
+
+	ASSERT_TRUE(&m[1][1] != &m1[1][1]);
+}
+
+TEST(TMatrix, can_get_size)
+{
+	TMatrix<int> m(15);
+
+	EXPECT_EQ(15, m.GetSize());
+}
+
+TEST(TMatrix, can_set_and_get_element)
+{
+	TMatrix<int> m(10);
+	int a;
+	m[1][2] = 5;
+	a = m[1][2];
+
+	ASSERT_TRUE(a == 5);
+}
+
+TEST(TMatrix, throws_when_set_element_with_negative_index)
+{
+	TMatrix<int> m(10);
+
+	ASSERT_ANY_THROW(m[2][-1] = 11);
+}
+
+TEST(TMatrix, throws_when_set_element_with_too_large_index)
+{
+	TMatrix<int> m(10);
+
+	ASSERT_ANY_THROW(m[2][100] = 11);
+}
+
+TEST(TMatrix, can_assign_matrix_to_itself)
+{
+	TMatrix<int> m(10);
+
+	ASSERT_NO_THROW(m = m);
+}
+
+TEST(TMatrix, can_assign_matrices_of_equal_size)
+{
+	TMatrix<int> m(10);
+	TMatrix<int> m1(10);
+
+	ASSERT_NO_THROW(m = m1);
+}
+
+TEST(TMatrix, assign_operator_change_matrix_size)
+{
+  TMatrix<int> v1(10);
+  TMatrix<int> v2(20);
+  
+  v1 = v2;
+
+  ASSERT_TRUE(v1.GetSize() == v2.GetSize());
+}
+
+TEST(TMatrix, can_assign_matrices_of_different_size)
+{
+	TMatrix<int> m(20);
+	TMatrix<int> m1(10);
+
+	ASSERT_NO_THROW(m = m1);
+}
+
+TEST(TMatrix, compare_equal_matrices_return_true)
+{
+	TMatrix<int> m(10);
+	TMatrix<int> m1(10);
+
+	ASSERT_TRUE(m == m1);
+}
+
+TEST(TMatrix, compare_matrix_with_itself_return_true)
+{
+	TMatrix<int> m(10);
+
+	ASSERT_TRUE(m == m);
+}
+
+TEST(TMatrix, matrices_with_different_size_are_not_equal)
+{
+	TMatrix<int> m(10);
+	TMatrix<int> m1(20);
+
+	ASSERT_TRUE(m != m1);
+}
+
+TEST(TMatrix, can_add_matrices_with_equal_size)
+{
+	TMatrix<int> m(20);
+	m[1][3] = 2;
+	TMatrix<int> m1(20);
+	m1[3][5] = 30;
+	ASSERT_NO_THROW(m + m1);
+}
+
+TEST(TMatrix, cant_add_matrices_with_not_equal_size)
+{
+	TMatrix<int> m(30);
+	m[1][3] = 2;
+	TMatrix<int> m1(20);
+	m1[3][5] = 30;
+	ASSERT_ANY_THROW(m + m1);
+}
+
+TEST(TMatrix, can_subtract_matrices_with_equal_size)
+{
+	TMatrix<int> m(20);
+	m[1][3] = 2;
+	TMatrix<int> m1(20);
+	m1[3][5] = 30;
+	ASSERT_NO_THROW(m - m1);
+}
+
+TEST(TMatrix, cant_subtract_matrixes_with_not_equal_size)
+{
+	TMatrix<int> m(30);
+	m[1][3] = 2;
+	TMatrix<int> m1(20);
+	m1[3][5] = 30;
+	ASSERT_ANY_THROW(m - m1);
+}
+
